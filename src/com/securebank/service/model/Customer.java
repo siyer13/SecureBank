7447c419f1b84348b618fe1dd019b3aa5c084b81
Sridhar <sreedharriyer@gmail.com>
2016-07-07 21:36:31 -0700

    adding model

---
+++ src/com/securebank/service/model/Customer.java
@@ -0,0 +1,91 @@
+package com.securebank.cxfrestservice.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="customer")
+public class Customer {
+	
+	private String customerId;
+	private String firstName;
+	private String lastName;
+	private String ssn;
+	private Date dateOfBirth;
+	private String phoneNumber;
+	private String emailAddress;
+	private String homeAddress;
+	private List<Account> accounts;
+	
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getHomeAddress() {
+		return homeAddress;
+	}
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}	
+	
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", firstName=" + firstName + ", lastName=" + lastName + ", ssn="
+				+ ssn + ", dateOfBirth=" + dateOfBirth + ", phoneNumber=" + phoneNumber + ", emailAddress="
+				+ emailAddress + ", homeAddress=" + homeAddress + ", accounts=" + accounts + "]";
+	}
+
+
+
+	public static class CustomerAccount {
+		
+	}
+}
+
+
+
